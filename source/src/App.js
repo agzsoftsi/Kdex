8e5c323fd841e526cf5d9bfed3ab83bb2ebbecc8
Carlos Garcia <agzsoftsi@gmail.com>
2022-08-05 16:23:03 -0500

    main code

---
+++ source/src/App.js
@@ -0,0 +1,14 @@
+import './App.scss';
+import DexLayout from './components/DexLayout/DexLayout';
+
+function App() {
+  return (
+    <div className="app">
+      <DexLayout>
+
+      </DexLayout>
+    </div>
+  );
+}
+
+export default App;
---
+++ source/src/components/DexLayout/DexLayout.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './DexLayout.scss'
+
+function DexLayout() {
+  return (
+    <div className='dexlayout'>container</div>
+  )
+}
+
+export default DexLayout
